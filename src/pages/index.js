51a02b906a66318db988b7b48eda7f224def8bfe
[Mehdi Hellal] <[hellalm@gmatc.matc.edu]>
2019-03-11 22:23:25 -0500

    Add styled-icon

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
 import React from "react"
 import { Link } from "gatsby"
-import {Button} from '../components/Button'
+import {Button, IconButton} from '../components/Button'
+
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import { ReactLogo } from 'styled-icons/fa-brands/ReactLogo'
+
 
 const IndexPage = () => (
   <Layout>
@@ -13,6 +16,9 @@ const IndexPage = () => (
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Button>Test</Button>
+     <br/>
+     <IconButton icon= {<ReactLogo/>}/>
+     <br/>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
